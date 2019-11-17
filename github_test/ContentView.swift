d69eddd414150ea607f8654bfa4ae41d53f48977
Giriraj <giriraj1310@gmail.com>
2019-11-16 20:20:55 -0500

    Adding HStack and an Image

--- github_test/ContentView.swift
+++ github_test/ContentView.swift
@@ -10,7 +10,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        HStack{
+            Image(systemName:"phone")
+            Text("647-231-5310")
+        }
     }
 }
 
